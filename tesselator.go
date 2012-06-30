804889033328d24e415c1188c56445fb2f9550d0
Carl Chatfield <carlchatfield@gmail.com>
2012-06-30 13:01:27 +1000

    Added some basic documentation.

--- tesselator.go
+++ tesselator.go
@@ -9,6 +9,7 @@ import "C"
 import "github.com/banthar/gl"
 import "unsafe"
 
+// Opaque object used for book keeping on the go side.
 type Tesselator struct {
 	tess *C.GLUtesselator
 
@@ -26,11 +27,13 @@ type Tesselator struct {
 	combineData    TessCombineHandler
 }
 
+// Wrapper around an interface. Does go not support (*interface{})(ptr)?
 type vertexDataWrapper struct {
 	data interface{}
 }
 
 
+// Create a new tesselator.
 func NewTess() (tess *Tesselator) {
 	tess = new(Tesselator)
 	tess.tess = C.gluNewTess()
@@ -42,34 +45,44 @@ func NewTess() (tess *Tesselator) {
 	return
 }
 
+// Clean up resources held by the tesselator. Go's garbage collector cannot
+// do this automatically.
 func (tess *Tesselator) Delete() {
 	C.gluDeleteTess(tess.tess)
 	tess.tess = nil
 }
 
+// Begin the drawing of the polygon, with the data parameter that will
+// be provided to callbacks.
 func (tess *Tesselator) BeginPolygon(data interface{}) {
 	tess.polyData = data
 	C.gluTessBeginPolygon(tess.tess, unsafe.Pointer(tess))
 }
 
+// End the drawing of the polygon.
 func (tess *Tesselator) EndPolygon() {
 	C.gluTessEndPolygon(tess.tess)
 }
 
+// Begin a contour within the polygon.
 func (tess *Tesselator) BeginContour() {
 	C.gluTessBeginContour(tess.tess)
 }
 
+// End a contour within the polygon.
 func (tess *Tesselator) EndContour() {
 	C.gluTessEndContour(tess.tess)
 }
 
+// Add a vertex to the polygon, with the data parameter that will be
+// provided to callbacks.
 func (tess *Tesselator) Vertex(location [3]float64, data interface{}) {
 	_data := &vertexDataWrapper{data}
 	tess.vertData = append(tess.vertData, _data)
 	C.gluTessVertex(tess.tess, (*C.GLdouble)(&location[0]), unsafe.Pointer(_data))
 }
 
+// Set the normal of the plane onto which points are projected onto before tesselation.
 func (tess *Tesselator) Normal(valueX, valueY, valueZ float64) {
 	cx := C.GLdouble(valueX)
 	cy := C.GLdouble(valueY)
@@ -77,6 +90,7 @@ func (tess *Tesselator) Normal(valueX, valueY, valueZ float64) {
 	C.gluTessNormal(tess.tess, cx, cy, cz)
 }
 
+// Set a property of the tesselator.
 func (tess *Tesselator) Property(which gl.GLenum, data float64) {
 	C.gluTessProperty(tess.tess, C.GLenum(which), C.GLdouble(data))
 }
